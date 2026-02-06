525ff3dfbb8a52c529799ea1cbf3558d80ec7498
Assaf Koren <assafnoahkoren@gmail.com>
2026-02-06 19:09:26 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 const char* WIFI_SSID = "Asaf";
 const char* WIFI_PASS = "122122122";
@@ -20,6 +21,17 @@ void setup() {
     Serial.println("Connected!");
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+
+    HTTPClient http;
+    http.begin("https://webhook.site/81614a16-577c-4ac6-bcd7-0732ed262830");
+    http.addHeader("Content-Type", "application/json");
+
+    int httpCode = http.POST("{\"device\":\"ESP32-S3\",\"message\":\"Hello from ESP32!\",\"uptime_ms\":" + String(millis()) + "}");
+
+    Serial.print("HTTP response: ");
+    Serial.println(httpCode);
+
+    http.end();
 }
 
 void loop() {
