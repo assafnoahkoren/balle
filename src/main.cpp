75a68173f42b790afaf7a1cbddbd6ba605a2c303
Assaf Koren <assafnoahkoren@gmail.com>
2026-02-06 19:07:17 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+const char* WIFI_SSID = "Asaf";
+const char* WIFI_PASS = "122122122";
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
-    Serial.println("Hello World from ESP32!");
+
+    Serial.print("Connecting to WiFi");
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.println("Connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
-    Serial.println("Still alive...");
     delay(2000);
 }
